ab9e16f08c976628f26b2c3de37643e56186dc1a
Mel <potatocanon606@gmail.com>
2023-02-16 18:37:55 -0500

    Added functionality to add column to grid

--- script.js
+++ script.js
@@ -5,13 +5,26 @@ let colorSelected;
 
 // Add a row
 function addR() {
-    
+  numRows++;
+  let table = document.getElementById("grid"); // Get the table
+  let col = table.rows[0].cells.length;  // Get the number of columns
+  let newRow = table.insertRow(-1); // Insert a row at the end of the table
+  for (let i = 0; i < col; i++) { // Loop through the number of columns
+    newRow.insertCell(i); // Insert a cell at the end of the row and the length of the number of columns
+  }
 }
 
 // Add a column
 function addC() {
-    alert("Clicked Add Col"); // Replace this line with your code.
+  numCols++;
+  let table = document.getElementById("grid"); // Get the table
+  let row = table.rows.length; // Get the number of rows
+  for (let i = 0; i < row; i++) { // Loop through the number of rows
+    table.rows[i].insertCell(i); // Insert a cell at the end of the columns and the length of the number of rows 
+    //Can have -1 or i for the second parameter of insertCell() to insert a cell at the beginning of the row but it relies on length of the rows
+  }
 }
+
 
 // Remove a row
 function removeR() {
