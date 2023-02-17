72a4c66bac402f68396305edcc3629b23ec3d9a7
Mel <potatocanon606@gmail.com>
2023-02-16 19:09:46 -0500

    Added feature to clear out all the cells of color

--- script.js
+++ script.js
@@ -58,5 +58,10 @@ function fillAll(){
 
 // Clear all cells
 function clearAll(){
-    alert("Clicked Clear All"); // Replace this line with your code.
+  let table = document.getElementById("grid");
+  for (let i = 0; i < table.rows.length; i++){ 
+    for (let j = 0; j < table.rows[i].cells.length; j++){
+      table.rows[i].cells[j].style.backgroundColor = "";
+    }
+  }
 }
