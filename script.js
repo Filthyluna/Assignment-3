d11ed1200b0b0987010e95ab5b92c02e0b2c8fc7
Mel <potatocanon606@gmail.com>
2023-02-18 15:59:38 -0500

    Debugged issue with columns not appearing at the end of the table

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 // Declare global variables
-let numRows = 0;
-let numCols = 0;
+let numRows = 2;
+let numCols = 4;
 let colorSelected; 
+let table = document.getElementById("grid"); // Get the table
 
 // Add a row
 function addR() {
-  numRows++;
-  let table = document.getElementById("grid"); // Get the table
   let col = table.rows[0].cells.length;  // Get the number of columns
-  let newRow = table.insertRow(-1); // Insert a row at the end of the table
-  for (let i = 0; i < col; i++) { // Loop through the number of columns
-    newRow.insertCell(i); // Insert a cell at the end of the row and the length of the number of columns
+  if (numCols === 0) {
+    addC();
+  }
+  else {
+    let newRow = table.insertRow(-1); // Insert a row at the end of the table
+    for (let i = 0; i < col; i++) { // Loop through the number of columns
+      newRow.insertCell(i); // Insert a cell at the end of the row and the length of the number of columns
+    }
+    numRows++;
   }
 }
 
 // Add a column
 function addC() {
-  numCols++;
-  let table = document.getElementById("grid"); // Get the table
   let row = table.rows.length; // Get the number of rows
-  for (let i = 0; i < row; i++) { // Loop through the number of rows
-    table.rows[i].insertCell(i); // Insert a cell at the end of the columns and the length of the number of rows 
-    //Can have -1 or i for the second parameter of insertCell() to insert a cell at the beginning of the row but it relies on length of the rows
+  if (numRows === 0) {
+    addR()
+  }
+  else {
+    for (let i = 0; i < row; i++) { // Loop through the number of rows
+      table.rows[i].insertCell(-1); // Insert a cell at the end of the columns and the length of the number of rows 
+      //Can have -1 or i for the second parameter of insertCell() to insert a cell at the beginning of the row but it relies on length of the rows
+    }
+    numCols++;
   }
 }
 
 
 // Remove a row
 function removeR() {
-  let table = document.getElementById("grid");
   let row_remove = table.rows.length - 1;
   table.deleteRow(row_remove);
   numRows--;
@@ -36,7 +44,6 @@ function removeR() {
 
 // Remove a column
 function removeC() {
-  let table = document.getElementById("grid");
   for(let i = 0; i < table.rows.length; i++){
     table.rows[i].deleteCell(-1);
   }
@@ -50,7 +57,6 @@ function selectColor(){
 
 // Fill all uncolored cells
 function fillU(){
-  let table = document.getElementById("grid");
   for (let i = 0; i < table.rows.length; i++){
     for (let j = 0; j < table.rows[i].cells.length; j++){
       if (table.rows[i].cells[j].style.backgroundColor == ""){
@@ -62,7 +68,6 @@ function fillU(){
 
 // Fill all cells
 function fillAll(){
-  let table = document.getElementById("grid");
   for (let i = 0; i < table.rows.length; i++){
     for (let j = 0; j < table.rows[i].cells.length; j++){
       table.rows[i].cells[j].style.backgroundColor = colorSelected;
@@ -71,22 +76,20 @@ function fillAll(){
 }
 
 function fillOne(){
-  let table = document.getElementById("grid");
   for (let i = 0; i < table.rows.length; i++){
     for (let j = 0; j < table.rows[i].cells.length; j++){
-      table.rows[i].cells[j].onclick = function(){
-        this.style.backgroundColor = colorSelected;
-      }
+      table.rows[i].cells[j].addEventListener('click', function (e) {
+        e.target.style.backgroundColor = colorSelected;
+      });
     }
   }
 }
 
 // Clear all cells
 function clearAll(){
-  let table = document.getElementById("grid");
   for (let i = 0; i < table.rows.length; i++){ 
     for (let j = 0; j < table.rows[i].cells.length; j++){
-      table.rows[i].cells[j].style.backgroundColor = "";
+      table.rows[i].cells[j].style.backgroundColor = "White";
     }
   }
 }
